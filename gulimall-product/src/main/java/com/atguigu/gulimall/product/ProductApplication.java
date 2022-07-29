e307578e42334fce6b95df05f14ee9d3b5f57d5c
Alex119793898 <119793898@qq.com>
2022-07-29 19:43:13 +0800

    重新生成 gulimall-product 配置数据库 / 配置mybatis

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/ProductApplication.java
@@ -0,0 +1,22 @@
+package com.atguigu.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* 整合Mybatis-plus
+*   1.导入Mybatis-plus
+*   2.配置
+*       配置数据库
+*           a.导入数据库驱动
+*           b.配置数据源
+*        配置mybatis-plus
+*           a.使用MapperScan
+*           b.告诉mybatis-plus,sql映射文件位置
+* */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
