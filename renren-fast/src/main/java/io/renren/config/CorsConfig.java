91916ea91866c198979a6681f93b3ab832928260
Alex119793898 <119793898@qq.com>
2022-08-02 11:22:05 +0800

    gateway模块 增加跨域配置 renren-fast 取消跨域配置

--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -9,18 +9,21 @@
 package io.renren.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+    //allowedOriginPatterns
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOriginPatterns("*")
+        CorsRegistration corsRegistration = registry.addMapping("/**");
+        corsRegistration
+            .allowedOrigins("*")
             .allowCredentials(true)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .maxAge(3600);
-    }
+    }*/
 }
